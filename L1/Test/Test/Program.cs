e2382fe64cf21bd7c6cad545cf21134d2b9ff306
Oksana <opolishchuk89@outlook.com>
2019-10-15 21:55:06 +0300

    Create Test project

---
+++ L1/Test/Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Oksana the best");
+            System.Console.WriteLine(Add(5,4));
+            System.Console.WriteLine(Text("Oksana"));
+        }
+
+        static int Add(int a, int b)
+        {
+            int c;
+            c = a * 3;
+            return c + b;
+        }
+
+        static string Text(string name)
+        {
+            return "hello " + name;
+        }
+    }
+}
