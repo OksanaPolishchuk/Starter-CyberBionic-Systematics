9d00b3fe5cb0b74074f29e6590d38820c97992ea
Oksana <opolishchuk89@outlook.com>
2019-10-28 18:57:23 +0200

    Lesson3Ex2

--- L1/Lesson3Ex2/Lesson3Ex2/Program.cs
+++ L1/Lesson3Ex2/Lesson3Ex2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Lesson3Ex2
 {
@@ -10,6 +7,15 @@ namespace Lesson3Ex2
     {
         static void Main(string[] args)
         {
+            int a = 7;
+            int b = 9;
+            int c = 4;
+
+            int sum = a + b +c;
+
+            Console.WriteLine((double)sum / 3);
+
+            Console.ReadKey();
         }
     }
 }
