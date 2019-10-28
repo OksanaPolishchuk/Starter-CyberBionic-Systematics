ee56bc3942d3353fdcba787d41bf4a89b28a24da
Oksana <opolishchuk89@outlook.com>
2019-10-29 00:44:41 +0200

    L3Ex4

--- L1/lesson3Ex4/lesson3Ex4/Program.cs
+++ L1/lesson3Ex4/lesson3Ex4/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace lesson3Ex4
@@ -10,6 +7,32 @@ namespace lesson3Ex4
     {
         static void Main(string[] args)
         {
+            // v = pi*r2*h
+            // s = 2*pi*r2+2*pi*r*h = 2*pi*r(r+h)
+
+            const double pi = 3.14;
+            byte h = 2; 
+            double r = 4, y = 2;
+            double result = Math.Pow(r, y);
+
+            double v = pi * result * h;
+
+            Console.WriteLine(v);
+
+            // s = 2*pi*r(r+h)
+
+            double s1 = 2 * pi * r * (r + h);
+
+            Console.WriteLine(s1);
+
+            // s = 2*pi*r2+2*pi*r*h 
+
+            double s2 = 2 * pi * result + 2 * pi * r * h;
+
+            Console.WriteLine(s2);
+
+            Console.ReadKey();
+
         }
     }
 }
