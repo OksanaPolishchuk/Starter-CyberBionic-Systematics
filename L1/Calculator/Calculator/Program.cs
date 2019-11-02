f9deb307aab2317de605a4b99e97bebe0993ac35
Oksana <opolishchuk89@outlook.com>
2019-11-03 01:23:35 +0200

    Calculator

--- L1/Calculator/Calculator/Program.cs
+++ L1/Calculator/Calculator/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator
@@ -10,6 +7,40 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int operand1 = 2, operand2 = 0;
+            Console.Write("Выберите знак арифметической операции: +, -, *, /.");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                {
+                    Console.WriteLine(operand1 + operand2);
+                    break;
+                }
+                case "-":
+                {
+                    Console.WriteLine(operand1 - operand2);
+                    break;
+                }
+                case "*":
+                {
+                    Console.WriteLine(operand1 * operand2);
+                    break;
+                }
+                case "/":
+                    if (operand2 > 0)
+                    {
+                        Console.WriteLine(operand1 / operand2);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }  break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
