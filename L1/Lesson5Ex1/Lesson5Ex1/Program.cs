40673c465e564437bbb83e672c1c384c10d776af
Oksana <opolishchuk89@outlook.com>
2019-11-05 14:05:31 +0200

    Lesson5Ex1

--- L1/Lesson5Ex1/Lesson5Ex1/Program.cs
+++ L1/Lesson5Ex1/Lesson5Ex1/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lesson5Ex1
 {
     class Program
+    
     {
         static void Main(string[] args)
-        {
+        { 
+            byte n = 2, x = 0;
+            Console.Write("Insert a number");
+            string c = Console.ReadLine();
+            int a = Convert.ToInt32(c);
+
+            //Method1
+            if ((a & (a - 1)) == 0)
+                Console.WriteLine("Method1: The indicated number is a power of 2"); 
+            else 
+                Console.WriteLine("Method1: The indicated number is not a power of 2");
+
+            //Method2
+            int z = a; 
+            do
+            {
+                z = z / 2;
+                if (z == 2) 
+                    Console.WriteLine("Method2: The indicated number is a power of 2");
+                else if (z == 0) 
+                    Console.WriteLine("Method2: The indicated number is not a power of 2"); 
+            }
+            while (z != 2 && z != 0);
+
+            Console.ReadKey();
         }
     }
 }
