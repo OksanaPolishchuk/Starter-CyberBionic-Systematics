18a7b46b933b80077cb96ea184deeec68da849b1
Oksana <opolishchuk89@outlook.com>
2019-11-06 00:38:26 +0200

    lesson5Ex2Additional

--- L1/lesson5Ex2Additional/lesson5Ex2Additional/Program.cs
+++ L1/lesson5Ex2Additional/lesson5Ex2Additional/Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace lesson5Ex2Additional
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Method1
 
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+            Int32 z = Convert.ToInt32(a);
 
+            if (z % 2 == 0)
+                Console.WriteLine("Четное число");
+            else
+                Console.WriteLine("Нечетное число");
+
+            //Method2
+
+            int s = z;
+            if ((s & 1) == 0) 
+                Console.WriteLine("Четное число");
+            else
+                Console.WriteLine("Нечетное число");
+
+            Console.ReadKey();
         }
     }
 }
