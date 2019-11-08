902f5e088c771acc061b7a8d39d61a86507b51e1
Oksana <opolishchuk89@outlook.com>
2019-11-08 15:59:56 +0200

    Lesson5Ex3

--- L1/Lesson5Ex3/Lesson5Ex3/Program.cs
+++ L1/Lesson5Ex3/Lesson5Ex3/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lesson5Ex3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //x += y >> x++ * z;
+            {
+                int x = 5;
+                int y = 10;
+                int z = 15;
+
+                int result1 = ((x += y) >> x++) * z;
+
+                Console.WriteLine(result1);
+            }
+            //  z = ++x & y * 5;
+            {
+                int x = 5;
+                int y = 10;
+                int z = 15;
+
+                int result2 = (++x & y) * 5;
+
+                Console.WriteLine(result2);
+            }
+            //  y /= x + 5 | z;
+            {
+                int x = 5;
+                int y = 10;
+                int z = 15;
+
+                int result3 = (y /=(x + 5)) | z;
+
+                Console.WriteLine(result3);
+            }
+            // z = x++ & y * 5
+            {
+                int x = 5;
+                int y = 10;
+                int z = 15;
+
+                int result4 = (z = (x++ & y)) * 5;
+
+                Console.WriteLine(result4);
+            }
+            //x = y << x++ ^ z
+            {
+                int x = 5;
+                int y = 10;
+                int z = 15;
+
+                int result5 = (x = (y << x++)) ^ z;
+
+                Console.WriteLine(result5);
+            }
+            Console.ReadKey();
         }
     }
 }
