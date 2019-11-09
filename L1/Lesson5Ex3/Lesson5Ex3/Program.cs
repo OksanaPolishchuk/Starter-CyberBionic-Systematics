8235e732f7ccbd9e634a6bcb60c626b30affc3c1
Oksana <opolishchuk89@outlook.com>
2019-11-09 12:19:39 +0200

    L5Ex3Method2

--- L1/Lesson5Ex3/Lesson5Ex3/Program.cs
+++ L1/Lesson5Ex3/Lesson5Ex3/Program.cs
@@ -6,48 +6,42 @@ namespace Lesson5Ex3
     {
         static void Main(string[] args)
         {
-            /*   x += y >> x++ * z; 
+            /*  Method 2
+             
+                 x += y >> x++ * z; 
                  z = ++x & y * 5; 
                  y /= x + 5 | z; 
                  z = x++ & y * 5; 
                  x = y << x++ ^ z; */
-            {
-                int x = 5, y = 10, z = 15;
+
+            int x = 5, y = 10, z = 15;
 
                 x = x + y >> (x++ * z);
 
                 Console.WriteLine(x);
-            }
-            {
-                int x = 5, y = 10, z = 15;
 
                 z = ++x & (y * 5);
 
                 Console.WriteLine(z);
-            }
-            {
-                int x = 5, y = 10, z = 15;
-
-                y /= ((x + 5) | z);
+            
+            
+                y = y /= ((x + 5) | z);
 
                 Console.WriteLine(y);
-            }
-            {
-                int x = 5, y = 10, z = 15;
-
+            
+            
                 z = x++ & (y * 5);
 
                 Console.WriteLine(z);
-            }
-            {
-                int x = 5, y = 10, z = 15;
+           
+            
                 x = (y << x++) ^ z;
 
                 Console.WriteLine(x);
 
-                Console.ReadKey();
+            Console.ReadKey();
 
-            }
+            
         }
     }
 }
