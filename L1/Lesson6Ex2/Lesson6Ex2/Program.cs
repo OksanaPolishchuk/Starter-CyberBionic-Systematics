3138534fbbab1ed580f493ebfbcf44e4e7ee7e57
Oksana <opolishchuk89@outlook.com>
2019-11-13 19:58:42 +0200

    Lesson6Ex2

---
+++ L1/Lesson6Ex2/Lesson6Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
