a560fc4a156fd4079a489bb53a2fe57790c22e09
Oksana <opolishchuk89@outlook.com>
2019-11-13 20:22:38 +0200

    Lesson6Ex2

--- L1/Lesson6Ex2/Lesson6Ex2/Program.cs
+++ L1/Lesson6Ex2/Lesson6Ex2/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lesson6Ex2
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Введите первое значение (первое значение < второе значение)");
+            string z = Console.ReadLine();
+            double a;
+            double.TryParse(z, out a);
+
+            Console.WriteLine("Введите второе значение");
+            string x = Console.ReadLine();
+            double b;
+            double.TryParse(x, out b);
+
+            double summ = 0;
+            double c = 0;
+
+            for (c = a + 1; c < b; c++)
+            {
+                summ += c;
+                if (c % 2 - 0.1 > 0.000001)
+                {
+                    Console.WriteLine("Нечетное число {0}", c);
+                }
+            }
+            Console.WriteLine("Cумма всех чисел: {0}", summ);
+
+            Console.ReadKey();
+
         }
     }
 }
