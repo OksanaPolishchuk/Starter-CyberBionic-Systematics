63def7ab7182119d6e8e108a27ae643c6611fd14
Oksana <opolishchuk89@outlook.com>
2019-11-19 01:24:26 +0200

    Lesson6Ex4

--- L1/Lesson6Ex4/Lesson6Ex4/Program.cs
+++ L1/Lesson6Ex4/Lesson6Ex4/Program.cs
@@ -6,7 +6,21 @@ namespace Lesson6Ex4
     {
         static void Main()
         {
-          
+            Console.WriteLine("How many customers?");
+            string a = Console.ReadLine();
+            int customers = 0;
+            int.TryParse(a, out customers);
+            int delivery = 1;
+
+            Console.Write("Delivery options: {0}!=", customers);
+            do
+            {
+                delivery *= customers--;
+            }
+            while (customers > 0);
+            Console.WriteLine("{0}", delivery);
+
+            Console.ReadKey();
         }
     }
 }
