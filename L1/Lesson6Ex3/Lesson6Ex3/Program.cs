258d4c7d01b310c58e1134e37e3c1e2a0b0a7272
Oksana <opolishchuk89@outlook.com>
2019-11-19 22:41:31 +0200

    Lesson7Ex2

--- L1/Lesson6Ex3/Lesson6Ex3/Program.cs
+++ L1/Lesson6Ex3/Lesson6Ex3/Program.cs
@@ -105,7 +105,9 @@ namespace Lesson6Ex3
               Console.Write("\n");
             }
 
-               Console.Write("\n");
+            Console.Write("\n");
+
+            // ялинка
             Console.ForegroundColor = ConsoleColor.Green;
 
             for (int b = 0; b < 5; b++)
@@ -197,6 +199,78 @@ namespace Lesson6Ex3
                 Console.Write("\n");
             }
             Console.ReadKey();
+
+            //----------------------------------------------------------------
+            //листва
+
+            //може бути довільним числом
+            const int NUMBER_OF_SECTIONS = 3;
+
+            //number of lines in the 1st section
+            //може бути довільним числом
+            const int BASE_NUMBER_OF_LINES = 5;
+
+            //різниця в ширині між останньою лінією першої секції
+            //і першої лінії другої секціїї
+            //може бути довільним числом
+            const int BASE_NEXT_SECTION_SHORTENING = 1;
+
+            //ідея - обчислити ширину листви біля стовбуру = максимальну ширину листви
+            //для того щоб далі знаючи ширину довільної лінії листви
+            //обчислювати відступ для цієї лінії
+            //максимальна ширина обчилюється через задані кількість секцій
+            //і кіль
+            int sumOfWidthShortenings = 0;
+            int numberOfLines = 0;
+            for (int i = 0; i < NUMBER_OF_SECTIONS; ++i)
+            {
+                numberOfLines += BASE_NUMBER_OF_LINES + 2 * NUMBER_OF_SECTIONS;
+
+                //BASE_NEXT_SECTION_SHORTENING*4 - сумарна різниця між 
+                // ????
+                sumOfWidthShortenings += (BASE_NEXT_SECTION_SHORTENING * 2 + 2) * NUMBER_OF_SECTIONS;
+            }
+            int foliageWidth = numberOfLines * 2 - 1 - sumOfWidthShortenings;
+            //????
+            int firstLineElongation = (BASE_NUMBER_OF_LINES - 1) * 2 - 2 * BASE_NEXT_SECTION_SHORTENING;
+
+            for (int sectionIndex = 0; sectionIndex < NUMBER_OF_SECTIONS; ++sectionIndex)
+            {
+                for (int lineIndex = 0; lineIndex < BASE_NUMBER_OF_LINES + 2 * sectionIndex; ++lineIndex)
+                {
+                    int lineLength = lineIndex * 2 + 1 + sectionIndex * firstLineElongation;
+                    for (int i = 0; i < (foliageWidth - lineLength) / 2; ++i)
+                    {
+                        Console.Write(' ');
+                    }
+                    for (int i = 0; i < lineLength; ++i)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write('\n');
+                }
+            }
+
+            //стовбур
+
+            const double TRUNK_FOLIAGE_RATIO = 0.25; //може бути довільним числом
+            const double TRUNK_HEIGHT_WIDTH_RATIO = 0.5; //може бути довільним числом
+
+            int trunkWidth = Convert.ToInt32(foliageWidth * TRUNK_FOLIAGE_RATIO);
+            int trunkHeight = Convert.ToInt32(trunkWidth * TRUNK_HEIGHT_WIDTH_RATIO);
+            int trunkIndentation = (foliageWidth - trunkWidth) / 2;
+            for (int i = 0; i < trunkHeight; ++i)
+            {
+                for (int j = 0; j < trunkIndentation; ++j)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 0; j < trunkWidth; ++j)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
         }
     }
 }
---
+++ L1/Lesson7Ex2/Lesson7Ex2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lesson7Ex2
+{
+    class Program
+    {
+        static double Add(int a, int b, ref double sum)
+        {
+            return sum = a + b;
+        }
+        static double Sub(int a, int b, ref double sum)
+        {
+            return sum = a - b;
+        }
+        static double Mul(int a, int b, ref double sum)
+        {
+            return sum = a * b;
+        }
+        static double Div(int a, int b, ref double sum)
+        {
+            try
+            {
+                return sum = Convert.ToDouble(a) / Convert.ToDouble(b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero.");
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first value");
+            string z = Console.ReadLine();
+            int a = 0;
+            int.TryParse(z, out a);
+
+            Console.Write("Выберите знак арифметической операции: +, -, *, /.");
+            string sign = Console.ReadLine();
+
+            Console.WriteLine("Enter the second value");
+            string s = Console.ReadLine();
+            int b = 0;
+            int.TryParse(s, out b);
+
+            double sum = 0;
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        Add(a, b, ref sum);
+                        Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+                        break;
+                    }
+                case "-":
+                    {
+                        Sub(a, b, ref sum);
+                        Console.WriteLine("{0} - {1} = {2}", a, b, sum);
+                        break;
+                    }
+                case "*":
+                    {
+                        Mul(a, b, ref sum);
+                        Console.WriteLine("{0} * {1} = {2}", a, b, sum);
+                        break;
+                    }
+                case "/":
+                    {
+                        Div(a, b, ref sum);
+                        if (sum != 0) 
+                            Console.WriteLine("{0} / {1} = {2}", a, b, sum);
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
